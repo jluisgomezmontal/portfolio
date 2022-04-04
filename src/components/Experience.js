7e2460c461909fc68e8698831ee665f3cbdda7b5
jluisgomezmontal <montalgex18@gmail.com>
2022-04-04 09:18:29 -0500

    justify content

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,13 +8,13 @@ export const Experience = ({experienceRef}) => {
         <hr />
         <h3>Tata Consultancy Services <span>|</span> 2021- Present</h3>
         <p>
-            I'm react developer responsible for implementing the project requirements to improve the
-            existing features of the main site, using React. Support the production
+            I am responsible for implementing the project requirements to improve the existing features
+            and develop new features for the internal tool to manage employees. Support the production
             deployment on AWS and verifying the integrity for each server.
         </p>
         <h3>Freelancer <span>|</span> 2019- 2021</h3>
         <p>
-        I offered my Freelance services as a web and applications developer. Create projects from scratch taking the clients needs into custom web applications.
+          Develop web applications based on clients requirements, increase the traffic of clients' web applications by implementing and improving SEO, collect client requirements from the beginning and make backlog tasks.
         </p>
 
     </div>
